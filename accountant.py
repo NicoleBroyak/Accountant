5dd76325c3df22feadfa7ea898d67827b6e84db3
Nikola Broyak <nikolabroyak@gmail.com>
2021-08-27 13:31:08 +0200

    fixed bugs

--- accountant.py
+++ accountant.py
@@ -1,4 +1,5 @@
 import sys
+
 account_values = []
 account_comments = []
 account_balance = 0
@@ -7,133 +8,164 @@ operations_type = []
 operations = []
 inputlog = []
 mode = sys.argv[1]
-inputlog.append(mode)
-index = 0
-index_saldo = 0
-index_zakup = 0
-index_sprzedaz = 0
+index, index_balance, index_purch, index_sell, account_balance = 0, 0, 0, 0, 0
+
+# sciezki programu:
+# 1. Saldo:
+#     - jeżeli saldo mniejsze niż wypłata - "za mało pieniędzy"
+# 2. Zakup:
+#     - jeżeli brak towaru: - dodaj do słownika wraz z ilością sztuk
+#     - jeżeli towar już jest: - zwiększ stan magazynowy
+#     - jeżeli koszt zakupu większy od salda - "za mało pieniędzy"
+# 3. Sprzedaż:
+#     - jeżeli brak towaru lub towaru mniej niż sprzedawane: - błąd "brak towaru"
+#     - jeżeli towar jest: zmniejsz stan magazynowy
+
+def acc_input(mode):
+    if mode == "zakup" or mode == "sprzedaż":
+        item_name = input("Please type item name")
+        item_price = int(input("Please type item price"))
+        item_quantity = int(input("Please type item quantity"))
+        return mode, item_name, item_price, item_quantity
+    if mode == "saldo":
+        account_value = int(input("Please type operation value (in gr)"))
+        account_comment = input("Please type operation description")
+        if account_balance < account_value:
+            while account_balance < account_value:
+                account_value = int(input("Please type operation value (in gr)"))
+                account_comment = input("Please type operation description")
+        return mode, account_value, account_comment
+    
+
+def input_check(mode,item_name = "",item_price = 0, item_quantity = 0, account_value = 0):
+    if mode == "saldo":
+        if account_balance + account_value < 0:
+            while account_balance + account_value < 0:
+                account_value = int(input("ERROR Please type operation value (in gr)"))
+                account_comment = input("Please type operation description")
+        return account_value
+    if mode == "sprzedaż":
+        if item_name not in trade_items:
+            while item_name not in trade_items:
+                item_name = input("ERROR: Please type correct item name")
+        if item_price <= 0 or item_quantity <= 0 or trade_items[item_name] < item_quantity:
+            while item_price <= 0 or item_quantity <= 0 or trade_items[item_name] < item_quantity:
+                item_price = int(input("ERROR Please type correct item price"))
+                item_quantity = int(input("ERROR Please type corect item quantity"))
+        return item_name, item_price, item_quantity
+    if mode == "zakup":  
+        if item_price <= 0 or item_quantity <= 0 or account_balance < item_price*item_quantity:
+            while item_price <= 0 or item_quantity <= 0 or account_balance < item_price*item_quantity:
+                item_price = int(input("ERROR Please type correct item price"))
+                item_quantity = int(input("ERROR Please type corect item quantity")) 
+        return item_name, item_price, item_quantity
+
+def trade_items_check(mode):
+    if mode == "zakup":
+        if item_name not in trade_items:
+            trade_items[item_name] = item_quantity
+        else:
+            trade_items[item_name] += item_quantity
+    if mode == "sprzedaż":
+        trade_items[item_name] -= item_quantity
+    return
+
+def oper_append(index_mode, index, mode):
+    if mode == "saldo":
+        account_values.append(account_value)
+        account_comments.append(account_comment)
+        operations_type.append(mode)
+        operations.append(f"Operation in mode 'saldo' number {str(index_mode + 1)}:"
+                        f" {str(account_values[index_mode])} - "
+                        f"description: {account_comments[index]}")
+        return
+    if mode == "zakup":
+        operations_type.append(mode)
+        operations.append(f"Operation in mode 'zakup'" 
+                          f"number {str(index_mode + 1)}:"
+                          f"Item:{item_name} purchased {item_quantity} pcs for price {item_price}gr")
+        return
+    if mode == "sprzedaż":
+        operations_type.append(mode)
+        operations.append(f"Operation in mode 'sprzedaż' number {str(index_mode + 1)}:"
+                          f"Item:{item_name} sold {item_quantity} pcs for price {item_price}gr")
+        return
+
+def inputlog_extend(mode):
+    if mode == "saldo":
+        inputlog.extend(mode, account_values[-1], account_comments[-1])
+    if mode == "zakup":
+        inputlog.extend(mode, item_name, item_price, item_quantity)
+    if mode == "sprzedaż":
+        inputlog.extend(mode, item_name, item_price, item_quantity)
+    return
+
+def input_mode():
+    mode = input("Please type mode:")
+    while mode not in allowed_commands:
+        mode = input("Please type mode:")
+    return mode
 
 allowed_commands = "saldo", "sprzedaż", "zakup", "stop", "end"
 if mode == "saldo":
-    account_values.append(int(sys.argv[2]))
-    account_comments.append(sys.argv[3])
-    inputlog.append(int(sys.argv[2]))
-    inputlog.append(sys.argv[3])
-    operations_type.append(mode)
-    operations.append(f"Operation in mode 'saldo' number {str(index_saldo + 1)}:"
-                      f" {str(account_values[index_saldo])} - "
-                      f"description: {account_comments[index]}")
-    print(f"Operation in mode 'saldo' number {str(index_saldo + 1)}:"
-          f" {str(account_values[index_saldo])} - "
-          f"description: {account_comments[index]}")
-    account_balance += account_values[index]
-    index_saldo += 1
+    account_value = int(sys.argv[2])
+    account_comment = sys.argv[3]
+    account_value = input_check(mode, 0, 0, 0, account_value)
+    account_balance += account_value
+    oper_append(index_balance, index, mode)
+    inputlog.extend([mode, account_value, account_comment])
+    index_balance += 1
     index += 1
 if mode == "zakup":
-    inputlog.append(mode)
     item_name = sys.argv[2]
-    while item_name not in trade_items:
-        item_name = input("ERROR: Please type correct item name")
     item_price = int(sys.argv[3])
     item_quantity = int(sys.argv[4])
-    if item_price <= 0 or item_quantity <= 0 or account_balance < item_price*item_quantity:
-        while item_price <= 0 or item_quantity <= 0:
-            item_price = int(input("Please type item price"))
-            item_quantity = int(input("Please type item quantity"))
+    item_name, item_price, item_quantity = input_check(mode, item_name, item_price, item_quantity)
+    trade_items_check(mode)
+    inputlog.extend(mode, item_name, item_price, item_quantity)
     account_balance -= item_price * item_quantity
-    trade_items[item_name] = item_quantity
-    inputlog.append(item_name)
-    inputlog.append(item_price)
-    inputlog.append(item_quantity)
+    index_purch += 1
+    index += 1
 if mode == "sprzedaż":
-    inputlog.append(mode)
     item_name = sys.argv[2]
-    while item_name not in trade_items:
-        item_name = input("ERROR: Please type correct item name")
     item_price = int(sys.argv[3])
     item_quantity = int(sys.argv[4])
-    if item_price <= 0 or item_quantity <= 0 or account_balance < item_price*item_quantity:
-        while item_price <= 0 or item_quantity <= 0 or trade_items[item_name] < item_quantity:
-            item_name = input("Please type item name")
-            item_price = int(input("Please type item price"))
-            item_quantity = int(input("Please type item quantity"))
-    account_balance -= item_price * item_quantity
-    trade_items[item_name] = item_quantity
-    inputlog.append(item_name)
-    inputlog.append(item_price)
-    inputlog.append(item_quantity)
-mode = input("Please type mode:")
+    item_name, item_price, item_quantity = input_check(mode, item_name, item_price, item_quantity)
+    trade_items_check(mode)
+    inputlog.extend(mode, item_name, item_price, item_quantity)
+    account_balance += item_price * item_quantity
+    index_sell += 1
+    index += 1
 
 # End of argv dependent code
-
-while mode not in allowed_commands:
-    mode = input("Please type correct mode:")
+mode = input_mode()
 while mode in allowed_commands:
     if mode == "saldo":
-        inputlog.append(mode)
-        account_values.append(int(input("Please type operation value (in gr)")))
-        account_comments.append(input("Please type operation description"))
-        inputlog.append(account_values[-1])
-        inputlog.append(account_comments[-1])
-        print(f"Operation in mode 'saldo' number {str(index_saldo + 1)}:"
-              f" {str(account_values[index_saldo])} - "
-              f"description: {account_comments[index]}")
-        account_balance += account_values[index]
-        operations_type.append(mode)
-        operations.append(f"Value: {account_values[index]} - "
-                          f"Comment: {account_comments[index]}")
-        index_saldo += 1
+        mode, account_value, account_comment = acc_input(mode)
+        account_value = input_check(mode, 0, 0, 0, account_value)
+        account_balance += account_value
+        oper_append(index_balance, index, mode)
+        inputlog.extend([mode, account_value, account_comment])
+        index_balance += 1
         index += 1
-        mode = input("Please type mode:")
-        while mode not in allowed_commands:
-            mode = input("Please type correct mode:")
     if mode == "zakup":
-        inputlog.append(mode)
-        item_name = input("Please type item name")
-        item_price = int(input("Please type item price"))
-        item_quantity = int(input("Please type item quantity"))
-        if item_price <= 0 or item_quantity <= 0 or account_balance < item_price*item_quantity:
-            while item_price <= 0 or item_quantity <= 0 or account_balance < item_price*item_quantity:
-                item_name = input("ERROR: Please type item name")
-                item_price = int(input("Please type item price"))
-                item_quantity = int(input("Please type item quantity"))
+        mode, item_name, item_price, item_quantity = acc_input(mode)
+        item_name, item_price, item_quantity = input_check(mode, item_name, item_price, item_quantity)
+        trade_items_check(mode)
         account_balance -= item_price * item_quantity
-        trade_items[item_name] = item_quantity
-        inputlog.append(item_name)
-        inputlog.append(item_price)
-        inputlog.append(item_quantity)
-        operations_type.append(mode)
-        operations.append(f"Operation in mode 'zakup' number {str(index_zakup + 1)}:"
-                          f"Item:{item_name} purchased {item_quantity} pcs for price {item_price}gr")
+        inputlog.extend([mode, item_name, item_price, item_quantity])
+        oper_append(index_purch, index, mode)
+        index_purch += 1
         index += 1
-        index_zakup += 1
-        mode = input("Please type mode:")
-        while mode not in allowed_commands:
-            mode = input("Please type correct mode:")
     if mode == "sprzedaż":
-        inputlog.append(mode)
-        item_name = input("Please type item name")
-        while item_name not in trade_items:
-            item_name = input("ERROR: Please type correct item name")
-        item_price = int(input("Please type item price"))
-        item_quantity = int(input("Please type item quantity"))
-        if item_price <= 0 or item_quantity <= 0:
-            while item_price <= 0 or item_quantity <= 0 or trade_items[item_name] < item_quantity:
-                item_price = int(input("ERROR: Please type item price"))
-                item_quantity = int(input("Please type item quantity"))
+        mode, item_name, item_price, item_quantity = acc_input(mode)
+        item_name, item_price, item_quantity = input_check(mode, item_name, item_price, item_quantity)
+        trade_items_check(mode)
         account_balance += item_price * item_quantity
-        trade_items[item_name] -= item_quantity
-        inputlog.append(item_name)
-        inputlog.append(item_price)
-        inputlog.append(item_quantity)
-        operations_type.append(mode)
-        operations.append(f"Operation in mode 'sprzedaż' number {str(index_sprzedaz + 1)}:"
-                          f"Item:{item_name} sold {item_quantity} pcs for price {item_price}gr")
+        inputlog.extend([mode, item_name, item_price, item_quantity])
+        oper_append(index_sell, index, mode)
+        index_sell += 1
         index += 1
-        index_sprzedaz += 1
-        mode = input("Please type mode:")
-        while mode not in allowed_commands:
-            mode = input("Please type correct mode:")
     if mode == "stop":
         print(f"Number of operations: {index}\n")
         index_first = int(input("Please type first operation to view:"))
@@ -155,4 +187,5 @@ while mode in allowed_commands:
             print(i)
         mode = "end"
         break
+    mode = input_mode()
 
